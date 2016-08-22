2916d037b743adda8c31b7f1a292b5f4e3cfdb63
Damian Rajca <damekr@gmail.com>
2016-08-22 20:37:41 +0200

    Extracted function responsible for different dutes like getSizeofFile and getNameofFile

--- bacsrv/transfer.go
+++ bacsrv/transfer.go
@@ -8,16 +8,18 @@ import (
 	"io"
 	"os"
 )
+
+// BUFFERSIZE determines how big is piece of data that will be send in one frame
 const BUFFERSIZE = 1024
+
+
 
 func InitTransferServer(){
 	listener, err := net.Listen("tcp", "localhost:27001")
 	if err != nil {
 		fmt.Println("An error" + err.Error())
 	}
-
-	bufferFileName := make([]byte, 64)
-	bufferFileSize := make([]byte, 10)
+	
 	for {
 		connection, err := listener.Accept()
 		if err != nil {
@@ -25,86 +27,59 @@ func InitTransferServer(){
 		}else{
 			fmt.Println("New connection estabilished")
 		}
-		sizeBytes, err := connection.Read(bufferFileSize)
-		if err != nil {
-			fmt.Println("An error during read: "+err.Error())
-		}
-		fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
-		fmt.Printf("Received: %d\n", fileSize)
 
-		nameBytes, err := connection.Read(bufferFileName)
-		if err != nil {
-			fmt.Println("An error during read: "+err.Error())
-		}
-		fileName := strings.Trim(string(bufferFileName), ":")
-		fmt.Printf("Received: %s\n", fileName)
-
-		fmt.Println(sizeBytes, "bytes received")
-		fmt.Println(nameBytes, "bytes received")
-
+		fileSize := GetFileSize(connection)
+		fileName := GetFileName(connection)
 		// Part of receiving file
-		newFile, err := os.Create(fileName)
-		if err != nil {
-			panic(err.Error())
-		}
-		defer newFile.Close()
-		var receivedBytes int64
-
-		for {
-			if (fileSize - receivedBytes) < BUFFERSIZE {
-				io.CopyN(newFile, connection, (fileSize - receivedBytes))
-				connection.Read(make([]byte, (receivedBytes +BUFFERSIZE) - fileSize))
-				break
-			}
-			io.CopyN(newFile, connection, BUFFERSIZE)
-			receivedBytes += BUFFERSIZE
-		}
-		fmt.Println("Received file completely!")
+		ReceiveFile(fileSize, fileName, connection)
 
 		connection.Close()
 	}
 }
 
+// ReceiveFile is able to read data from buffer and save them in created file.
+// It also checks if retrived file is equeal to sent earlier in first chunks of data.
+func ReceiveFile(fileSize int64, fileName string, connection net.Conn){
+	newFile, err := os.Create(fileName)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer newFile.Close()
+	var receivedBytes int64
+	for {
+		if (fileSize - receivedBytes) < BUFFERSIZE {
+			io.CopyN(newFile, connection, (fileSize - receivedBytes))
+			connection.Read(make([]byte, (receivedBytes +BUFFERSIZE) - fileSize))
+			break
+		}
+		io.CopyN(newFile, connection, BUFFERSIZE)
+		receivedBytes += BUFFERSIZE
+		}
+	fmt.Println("Received file completely!")
+}
 
-// func Listener(){
-// 	connection, err := net.Dial("tcp", "localhost:27001")
-// 	if err != nil {
-// 		fmt.Println("Error listetning: ", err)
-// 		os.Exit(1)
-// 	}
-// 	defer connection.Close()
-// 	fmt.Println("Server started! Waiting for connections...")
-		// fmt.Println("Client connected")
-        // bufferFileName := make([]byte, 64)
-		// bufferFileSize := make([]byte, 10)
-	
-		// connection.Read(bufferFileSize)
-		// fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
-	
-		// connection.Read(bufferFileName)
-		// fileName := strings.Trim(string(bufferFileName), ":")
-		// fmt.Println("File name: ", fileName)
-		// if fileName == "" {
-		// 	fileName = "tmp.tar"
-		// }
+// GetFileSize gets file size from given buffer --> remember to send and receive data in proper order 
+func GetFileSize(connection net.Conn) int64{
+	bufferFileSize := make([]byte, 10)
+	_, err := connection.Read(bufferFileSize)
+	if err != nil {
+		fmt.Println("An error during read: "+err.Error())
+	}
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+	fmt.Printf("Received: %d\n", fileSize)
+	return fileSize
+}
 
-		// newFile, err := os.Create(fileName)
-	
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// defer newFile.Close()
-		// var receivedBytes int64
-	
-		// for {
-		// 	if (fileSize - receivedBytes) < BUFFERSIZE {
-		// 		io.CopyN(newFile, connection, (fileSize - receivedBytes))
-		// 		connection.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
-		// 		break
-		// 	}
-		// io.CopyN(newFile, connection, BUFFERSIZE)
-		// receivedBytes += BUFFERSIZE
-		// }
-		// fmt.Println("Received file completely!")
+// GetFileName returns filename from given connection --> remember to send data and read
+func GetFileName(connection net.Conn) string{
+	bufferFileName := make([]byte, 64)
+	_, err := connection.Read(bufferFileName)
+	if err != nil {
+		fmt.Println("An error during read: "+err.Error())
+	}
+	fileName := strings.Trim(string(bufferFileName), ":")
+	fmt.Printf("Received: %s\n", fileName)
+	return fileName
 
+}
 
