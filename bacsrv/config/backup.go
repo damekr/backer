8323a30edc453571481ba22ce79c0754abdbeb11
Damian Rajca <damekr@gmail.com>
2017-06-26 19:59:50 +0200

    Updated config related files

---
+++ bacsrv/config/backup.go
@@ -0,0 +1,13 @@
+package config
+
+// Backup specifies a backup
+type Backup struct {
+	Paths     []string `json:"paths"`
+	Excluded  []string `json:"excludedPaths"`
+	Retention string   `json:"retentionTime"`
+}
+
+type BackupTriggerMessage struct {
+	ClientName   string `json:"clientName"`
+	BackupConfig Backup
+}
