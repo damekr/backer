b61a0120049646666407fe5e95609e8b582efbb4
Damian Rajca <damekr@gmail.com>
2017-07-17 21:09:35 +0200

    Removed unused files

--- bacsrv/operations/operations.go
+++ bacsrv/operations/operations.go
@@ -7,6 +7,23 @@ import (
 	"github.com/damekr/backer/bacsrv/outprotoapi"
 )
 
+func preBackupChecks(paths []string, clntAddr string) ([]string, error) {
+	log.Debug("Starting executing prebackup checks...")
+	log.Debug("Checking if client repsponds")
+	hostname, err := SendHelloMessageToClient(clntAddr)
+	if err != nil {
+		log.Errorf("Client %s does not responds", clntAddr)
+		return nil, err
+	}
+	log.Debug("Client sent it's own hostname, and it is: ", hostname)
+	checkedPaths, err := outprotoapi.CheckPaths(clntAddr, paths)
+	if err != nil {
+		log.Error("An error ocurred during checking paths, error: ", err.Error())
+		return nil, err
+	}
+	return checkedPaths, nil
+}
+
 // SendHelloMessageToClient is responsible for proxing restapi reqests to clients
 func SendHelloMessageToClient(clntAddress string) (string, error) {
 	clntHostname, err := outprotoapi.SayHelloToClient(clntAddress)
